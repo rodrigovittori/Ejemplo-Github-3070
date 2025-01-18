8756f730367e0803bf7d81201398dd08c6553b06
Rodrigo Vittori <vittorirodrigo@gmail.com>
2025-01-18 16:43:06 -0300

    [M7.L2] - Actividad # 1 "Creando el Proyecto"
    Primer commit: publicamos la primer versión de nuestro código

---
+++ main.bat
@@ -0,0 +1,8 @@
+@echo off
+REM # Este programa es un ejemplo para nuestro curso de Unity
+REM # Hoy aprenderemos a publicar y actualizar nuestro código online
+REM # Para ello utilizaremos la plataforma GitHub
+
+REM # https://github.com/rodrigovittori/Ejemplo-Github-3070
+
+echo. ¡Hola mundo!
