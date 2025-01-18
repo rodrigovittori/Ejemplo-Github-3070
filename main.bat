7cd8efb3c36d7acc4965c67cd32545195462736f
Sxvm72 <santiago.aaron.villagran@outlook.com>
2025-01-18 14:35:40 -0600

    Update main.bat

--- main.bat
+++ main.bat
@@ -17,7 +17,7 @@ echo. Ahora: Quiero que practiquen recuperar (fetch), y copiar (fork)
 echo. para que aprendan a trabajar en equipo :)
 
 echo. Mensaje de [Eithan] para el grupo:   ""
-echo. Mensaje de [Santiago] para el grupo: ""
+echo. Mensaje de [Santiago] para el grupo: "Hola a todos, suerte con sus proyectos!"
 echo. Mensaje de [Gerardo] para el grupo:  ""
 echo. Mensaje de [Thiago] para el grupo:   ""
 echo. Mensaje de [Castiel] para el grupo:  ""
